c3c29fe77e65382250d3f43d4c710925818a6ee5
rnamboze <154571664+rnamboze@users.noreply.github.com>
2024-10-21 09:10:18 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,18 +4,6 @@ import streamlit as st
 model = AutoModelForCausalLM.from_pretrained("gpt2")
 tokenizer = AutoTokenizer.from_pretrained("gpt2")
 
-prompt = "GPT2 is a model developed by OpenAI."
-
-input_ids = tokenizer(prompt, return_tensors="pt").input_ids
-
-gen_tokens = model.generate(
-    input_ids,
-    do_sample=True,
-    temperature=0.9,
-    max_length=100,
-)
-gen_text = tokenizer.batch_decode(gen_tokens)[0]
-
 def main():
     st.title("Hugging Face Text Generator")
 
