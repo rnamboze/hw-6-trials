7f8bb9022a690c94d02fce8d295d51ecdcd3574d
rnamboze <154571664+rnamboze@users.noreply.github.com>
2024-10-21 09:08:00 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,10 +4,6 @@ import streamlit as st
 model = AutoModelForCausalLM.from_pretrained("gpt2")
 tokenizer = AutoTokenizer.from_pretrained("gpt2")
 
-prompt = "GPT2 is a model developed by OpenAI."
-
-input_ids = tokenizer(prompt, return_tensors="pt").input_ids
-
 gen_tokens = model.generate(
     input_ids,
     do_sample=True,
